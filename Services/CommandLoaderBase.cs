f89ee41abd42e1948c13c075b92eaa88f7d41f6b
Popeye Zhong <zongsoft@gmail.com>
2014-07-20 06:51:09 +0800

    1. 修改了 ICommandLoader 接口，为该接口新增了一个 IsLoaded 只读属性。 2. 新增了 CommandLoaderBase 抽象类。 3. 修改了 CommandExecutorBase 抽象类，将其中的 Loader 属性移除。 4. 修改了 CommandTreeNode 类，为其新增了一个 Loader 属性。 5. 修改了 FtpServer 类和 FtpCommandLoader 类。

---
+++ Services/CommandLoaderBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zongsoft.Services
+{
+	public abstract class CommandLoaderBase : ICommandLoader
+	{
+		#region 同步变量
+		private readonly object _syncRoot = new object();
+		#endregion
+
+		#region 成员字段
+		private bool _isLoaded;
+		#endregion
+
+		#region 公共属性
+		public bool IsLoaded
+		{
+			get
+			{
+				return _isLoaded;
+			}
+		}
+		#endregion
+
+		#region 公共方法
+		public void Load(CommandTreeNode node)
+		{
+			if(node == null || _isLoaded)
+				return;
+
+			lock(_syncRoot)
+			{
+				if(_isLoaded)
+					return;
+
+				_isLoaded = this.OnLoad(node);
+			}
+		}
+		#endregion
+
+		#region 抽象方法
+		/// <summary>
+		/// 执行加载命令的实际操作。
+		/// </summary>
+		/// <param name="node">待加载的命令树节点。</param>
+		/// <returns>如果加载成功则返回真(true)，否则返回假(false)。</returns>
+		protected abstract bool OnLoad(CommandTreeNode node);
+		#endregion
+	}
+}
--- Services/ICommandLoader.cs
+++ Services/ICommandLoader.cs
@@ -32,12 +32,26 @@ namespace Zongsoft.Services
 	/// <summary>
 	/// 提供命令加载的功能。
 	/// </summary>
+	/// <remarks>
+	///		<para>对使用者的提醒：命令加载器不能重复使用，即不要把一个<see cref="ICommandLoader"/>实例赋予不同的用例，因为<seealso cref="IsLoaded"/>属性与不同的用例是无关的。</para>
+	/// </remarks>
 	public interface ICommandLoader
 	{
+		/// <summary>
+		/// 获取一个值表示当前加载器是否已经加载完成。
+		/// </summary>
+		bool IsLoaded
+		{
+			get;
+		}
+
 		/// <summary>
 		/// 将命令加载到指定的命令树节点中。
 		/// </summary>
 		/// <param name="node">要挂载的命令树节点。</param>
+		/// <remarks>
+		///		<para>对实现者的提醒：应该确保该方法的实现是线程安全的。</para>
+		/// </remarks>
 		void Load(CommandTreeNode node);
 	}
 }
