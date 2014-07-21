917f646115071a9bc81a52d08f1abcda0c9eaf5f
yuxing <yxlvzx@163.com>
2014-07-21 16:39:12 +0800

    Revert "Revert "新增了 Transactions 命名空间及其相关的代码文件。""
    This reverts commit 503e499e76dc142c3f4e7d079090b47d50b5501f.

---
+++ Transactions/EnlistmentContext.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2014 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Zongsoft.Transactions
+{
+	public class EnlistmentContext
+	{
+		#region 成员字段
+		private EnlistmentPhase _phase;
+		private Transaction _transaction;
+		#endregion
+
+		#region 构造函数
+		internal EnlistmentContext(Transaction transaction, EnlistmentPhase phase)
+		{
+			_transaction = transaction;
+			_phase = phase;
+		}
+		#endregion
+
+		#region 公共属性
+		/// <summary>
+		/// 获取当前事务阶段。
+		/// </summary>
+		public EnlistmentPhase Phase
+		{
+			get
+			{
+				return _phase;
+			}
+		}
+
+		/// <summary>
+		/// 获取当前的事务对象。
+		/// </summary>
+		public Transaction Transaction
+		{
+			get
+			{
+				return _transaction;
+			}
+		}
+		#endregion
+	}
+}
---
+++ Transactions/Transaction.cs
@@ -0,0 +1,185 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2014 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Zongsoft.Transactions
+{
+	public class Transaction : IDisposable, IEquatable<Transaction>
+	{
+		#region 静态字段
+		private static readonly ThreadLocal<Stack<Transaction>> _locals = new ThreadLocal<Stack<Transaction>>(() => new Stack<Transaction>());
+		#endregion
+
+		#region 私有变量
+		private Queue<IEnlistment> _enlistments;
+		#endregion
+
+		#region 成员字段
+		private TransactionInformation _information;
+		#endregion
+
+		#region 构造函数
+		public Transaction()
+		{
+			//首先设置当前事务的父事务
+			_information = new TransactionInformation(Transaction.Current);
+
+			//将当前事务对象加入到事务栈中
+			_locals.Value.Push(this);
+
+			//创建本事务的登记集合
+			_enlistments = new Queue<IEnlistment>();
+		}
+		#endregion
+
+		#region 静态属性
+		public static Transaction Current
+		{
+			get
+			{
+				if(_locals.IsValueCreated)
+				{
+					var stack = _locals.Value;
+
+					if(stack.Count > 0)
+						return stack.Peek();
+				}
+
+				return null;
+				//return TransactionManager.Instance.Current;
+			}
+		}
+		#endregion
+
+		#region 公共属性
+		/// <summary>
+		/// 获取当前事务的附加信息。
+		/// </summary>
+		public TransactionInformation Information
+		{
+			get
+			{
+				return _information;
+			}
+		}
+		#endregion
+
+		#region 公共方法
+		/// <summary>
+		/// 向当前事务登记一个事务处理过程的回调。
+		/// </summary>
+		/// <param name="enlistment">处理事务处理过程的回调接口。</param>
+		public void Enlist(IEnlistment enlistment)
+		{
+			if(enlistment == null)
+				throw new ArgumentNullException("enlistment");
+
+			_enlistments.Enqueue(enlistment);
+
+			//如果当前事务内被首次登记才需要处理事务范围的预处理回调
+			if(!_enlistments.Contains(enlistment))
+				enlistment.OnEnlist(new EnlistmentContext(this, EnlistmentPhase.Prepare));
+		}
+
+		/// <summary>
+		/// 提交事务。
+		/// </summary>
+		public void Commit()
+		{
+			this.DoEnlistment(EnlistmentPhase.Commit);
+		}
+
+		/// <summary>
+		/// 回滚事务。
+		/// </summary>
+		public void Rollback()
+		{
+			this.DoEnlistment(EnlistmentPhase.Rollback);
+		}
+		#endregion
+
+		#region 私有方法
+		private void DoEnlistment(EnlistmentPhase phase)
+		{
+			if(_enlistments.Count < 1)
+				return;
+
+			while(_enlistments.Count > 0)
+			{
+				var enlistment = _enlistments.Dequeue();
+
+				enlistment.OnEnlist(new EnlistmentContext(this, phase));
+			}
+
+			while(_locals.Value.Count > 0)
+			{
+				var current = _locals.Value.Pop();
+
+				if(object.ReferenceEquals(current, this))
+					return;
+
+				current.Rollback();
+			}
+		}
+		#endregion
+
+		#region 处置方法
+		protected virtual void Dispose(bool disposing)
+		{
+			this.Rollback();
+		}
+
+		public void Dispose()
+		{
+			this.Dispose(true);
+		}
+		#endregion
+
+		#region 重写方法
+		public override bool Equals(object obj)
+		{
+			if(obj == null || obj.GetType() != this.GetType())
+				return false;
+
+			return object.ReferenceEquals(this, obj);
+		}
+		#endregion
+
+		#region 相等比较
+		bool IEquatable<Transaction>.Equals(Transaction other)
+		{
+			if(other == null)
+				return false;
+
+			return object.ReferenceEquals(this, other);
+		}
+		#endregion
+	}
+}
