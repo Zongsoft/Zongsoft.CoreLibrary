293d3cc2785d76ae7241a99bdf25bd613958ab9d
PopeyeZhong <zongsoft@gmail.com>
2014-07-25 17:01:47 +0800

    将项目的目标框架由.NET 4.0升级到.NET 4.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,18 +3,14 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Zongsoft.CoreLibrary")]
-#if TRIAL
-	[assembly: AssemblyDescription("Zongsoft.CoreLibrary(Trial Edition)")]
-#else
-	[assembly: AssemblyDescription("Zongsoft.CoreLibrary")]
-#endif
+[assembly: AssemblyDescription("Zongsoft.CoreLibrary")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zongsoft Corporation")]
 [assembly: AssemblyProduct("Zongsoft Core Library")]
-[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2003-2013. All rights reserved.")]
-[assembly: AssemblyTrademark("Zongsoft is registered trademarks or trademarks of Zongsoft Corporation in the P.R.C. and/or other countries.")]
+[assembly: AssemblyCopyright("Copyright(C) Zongsoft Corporation 2010-2014. All rights reserved.")]
+[assembly: AssemblyTrademark("Zongsoft is registered trademarks of Zongsoft Corporation in the P.R.C. and/or other countries.")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("3.1.1310.*")]
-[assembly: AssemblyFileVersion("3.1.1310.0")]
+[assembly: AssemblyVersion("3.2.1407.*")]
+[assembly: AssemblyFileVersion("3.2.1407.0")]
