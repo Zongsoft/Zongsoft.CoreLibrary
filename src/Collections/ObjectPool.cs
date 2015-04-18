2edbe873c21da33c49a7eb388d9f7d57bc580e84
PopeyeZhong <zongsoft@gmail.com>
2015-04-18 18:24:19 +0800

    修复了对ObjectDisposed的检测方式的错误；增加了对缓存项对象为空(null)的检测机制；其他小调整。

--- src/Collections/ObjectPool.cs
+++ src/Collections/ObjectPool.cs
@@ -1,6 +1,7 @@
 ﻿/*
  * Authors:
  *   邓祥云(X.Z. Deng) <627825056@qq.com>
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com> Last modified date: 2015-04-18
  *
  * Copyright (C) 2008-2012 Zongsoft Corporation <http://www.zongsoft.com>
  *
@@ -31,20 +32,20 @@ using System.Threading;
 
 namespace Zongsoft.Collections
 {
-    /// <summary>
-    /// 提供了一个线程安全的通用对象池的相关功能。
-    /// </summary>
-    public class ObjectPool<T> : MarshalByRefObject, IDisposable
-    {
-        #region 私有变量
-        private ConcurrentBag<T> _idles;
-        private Func<T> _creator;
-        private Action<T> _remover;
+	/// <summary>
+	/// 提供了一个线程安全的通用对象池的相关功能。
+	/// </summary>
+	public class ObjectPool<T> : MarshalByRefObject, IDisposable where T : class
+	{
+		#region 私有变量
+		private ConcurrentBag<T> _idles;
+		private Func<T> _creator;
+		private Action<T> _remover;
 		private int _maximumLimit;
-        private SemaphoreSlim _semaphore;
-        #endregion
+		private SemaphoreSlim _semaphore;
+		#endregion
 
-        #region 构造函数
+		#region 构造函数
 		protected ObjectPool() : this(0)
 		{
 		}
@@ -58,14 +59,14 @@ namespace Zongsoft.Collections
 				_semaphore = new SemaphoreSlim(_maximumLimit, _maximumLimit);
 		}
 
-        /// <summary>
-        /// 创建一个新的对象管理池。
-        /// </summary>
-        /// <param name="creator">对象的创建方法。</param>
-        /// <param name="remover">对象移除时的回调，该参数值可以为空(null)。</param>
-        public ObjectPool(Func<T> creator, Action<T> remover) : this(creator, remover, 0)
-        {
-        }
+		/// <summary>
+		/// 创建一个新的对象管理池。
+		/// </summary>
+		/// <param name="creator">对象的创建方法。</param>
+		/// <param name="remover">对象移除时的回调，该参数值可以为空(null)。</param>
+		public ObjectPool(Func<T> creator, Action<T> remover) : this(creator, remover, 0)
+		{
+		}
 
 		/// <summary>
 		/// 创建一个新的对象管理池。
@@ -74,19 +75,19 @@ namespace Zongsoft.Collections
 		/// <param name="remover">对象移除时的回调，该参数值可以为空(null)。</param>
 		/// <param name="maximumLimit">对象池的最大容量，如果小于一则表示不控制池的大小。</param>
 		public ObjectPool(Func<T> creator, Action<T> remover, int maximumLimit)
-        {
-            if (creator == null)
-                throw new ArgumentNullException("creator");
+		{
+			if(creator == null)
+				throw new ArgumentNullException("creator");
 
-            _creator = creator;
-            _remover = remover;
+			_creator = creator;
+			_remover = remover;
 			_maximumLimit = Math.Max(maximumLimit, 0);
-            _idles = new ConcurrentBag<T>();
+			_idles = new ConcurrentBag<T>();
 
 			if(_maximumLimit > 0)
 				_semaphore = new SemaphoreSlim(_maximumLimit, _maximumLimit);
-        }
-        #endregion
+		}
+		#endregion
 
 		#region 公共属性
 		/// <summary>
@@ -96,99 +97,134 @@ namespace Zongsoft.Collections
 		{
 			get
 			{
+				var idles = _idles;
+
+				if(idles == null)
+					throw new ObjectDisposedException(null);
+
 				if(_semaphore == null || _maximumLimit < 1)
 					return -1;
 
-				return _maximumLimit - _idles.Count;
+				return _maximumLimit - idles.Count;
 			}
 		}
 
-        /// <summary>
-        /// 获取对象池的最大容量，零表示不限制。
-        /// </summary>
+		/// <summary>
+		/// 获取对象池的最大容量，零表示不限制。
+		/// </summary>
 		public int MaximumLimit
-        {
+		{
 			get
 			{
 				return _maximumLimit;
 			}
-        }
-        #endregion
+		}
+		#endregion
 
 		#region 公共方法
 		/// <summary>
-        /// 从对象池中获取一个可用对象。
-        /// </summary>
+		/// 从对象池中获取一个可用对象。
+		/// </summary>
 		public T GetObject()
 		{
-			//检查当前对象池是否被Disposed，如果是则抛出异常
-			TryThrowObjectDisposedException();
+			var idles = _idles;
+
+			if(idles == null)
+				throw new ObjectDisposedException(null);
 
 			T item;
 
 			if(_semaphore != null)
 				_semaphore.Wait();
 
-			if(!_idles.TryTake(out item))
-			{
+			if(!idles.TryTake(out item))
 				item = this.OnCreate();
+
+			//如果获取或者创建的新项为空，则释放一个信号量并返回空值
+			if(item == null)
+			{
+				if(_semaphore != null)
+					_semaphore.Release();
+
+				return null;
 			}
 
+			//回调取出方法
 			this.OnTakeout(item);
 
 			return item;
 		}
 
-        /// <summary>
-        /// 将一个对象释放到池中。
-        /// </summary>
+		/// <summary>
+		/// 将一个对象释放到池中。
+		/// </summary>
 		public void Release(T item)
 		{
-			//检查当前对象池是否被Disposed，如果是则抛出异常
-			TryThrowObjectDisposedException();
+			var idles = _idles;
 
+			if(idles == null)
+				throw new ObjectDisposedException(null);
+
+			if(item == null)
+				throw new ArgumentNullException("item");
+
+			//回调放入方法
 			this.OnTakein(item);
 
-			_idles.Add(item);
+			idles.Add(item);
 
 			if(_semaphore != null)
 				_semaphore.Release();
 		}
 
-        /// <summary>
-        /// 清空对象池，该方法会依次调用池中空闲对象的<see cref="OnRemove"/>方法。
-        /// </summary>
+		/// <summary>
+		/// 清空对象池，该方法会依次调用池中空闲对象的<see cref="OnRemove"/>方法。
+		/// </summary>
 		public void Clear()
 		{
-			if(_idles == null)
-				return;
+			var idles = _idles;
+
+			if(idles == null)
+				throw new ObjectDisposedException(null);
 
 			T item;
 
-			while(_idles.TryTake(out item))
+			while(idles.TryTake(out item))
 			{
 				this.OnRemove(item);
 			}
 		}
-        #endregion
+		#endregion
 
 		#region 虚拟方法
 		protected virtual T OnCreate()
 		{
 			if(_creator == null)
-				throw new InvalidOperationException();
+				return Activator.CreateInstance<T>();
 
 			return _creator();
 		}
 
+		/// <summary>
+		/// 表示将一个对象放入当前缓存池时该方法被调用。
+		/// </summary>
+		/// <param name="value">被放入的那个缓存项对象。</param>
 		protected virtual void OnTakein(T value)
 		{
 		}
 
+		/// <summary>
+		/// 表示当从当前缓存池中取出一个缓存项时该方法被调用。
+		/// </summary>
+		/// <param name="value">被取出的那个缓存项对象。</param>
 		protected virtual void OnTakeout(T value)
 		{
 		}
 
+		/// <summary>
+		/// 表示删除当前缓存池中的一个缓存项时该方法被调用。
+		/// </summary>
+		/// <param name="value">被删除的那个缓存项对象。</param>
 		protected virtual void OnRemove(T value)
 		{
 			if(_remover != null)
@@ -201,20 +237,12 @@ namespace Zongsoft.Collections
 		}
 		#endregion
 
-        #region 私有方法
-		private void TryThrowObjectDisposedException()
-		{
-			if(_idles == null)
-				throw new ObjectDisposedException(null);
-		}
-		#endregion
-
 		#region 释放资源
 		public void Dispose()
-        {
-            this.Dispose(true);
+		{
+			this.Dispose(true);
 			GC.SuppressFinalize(this);
-        }
+		}
 
 		protected virtual void Dispose(bool disposing)
 		{
@@ -223,16 +251,21 @@ namespace Zongsoft.Collections
 
 			Clear();
 
-			_idles = null;
-			_creator = null;
-			_remover = null;
+			var idles = System.Threading.Interlocked.Exchange(ref _idles, null);
 
-			if(_semaphore != null)
+			if(idles != null)
 			{
-				_semaphore.Dispose();
-				_semaphore = null;
+				_idles = null;
+				_creator = null;
+				_remover = null;
+
+				if(_semaphore != null)
+				{
+					_semaphore.Dispose();
+					_semaphore = null;
+				}
 			}
 		}
-        #endregion
-    }
+		#endregion
+	}
 }
