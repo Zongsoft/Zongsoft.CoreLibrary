b3d445e4a8b5c3e97416f9752f5fb5cd1aaa9512
Popeye Zhong <zongsoft@gmail.com>
2015-05-02 18:37:20 +0800

    为 HashSet 类写了一个 ToArray() 的扩展方法。

---
+++ src/Collections/HashSetExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zongsoft.Collections
+{
+	public static class HashSetExtension
+	{
+		public static T[] ToArray<T>(this HashSet<T> hashset)
+		{
+			if(hashset == null)
+				return null;
+
+			T[] result = new T[hashset.Count];
+			hashset.CopyTo(result);
+			return result;
+		}
+	}
+}
