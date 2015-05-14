fac21ef63fc6d783e3748297058d850c3087911d
Popeye Zhong <zongsoft@gmail.com>
2015-05-14 17:15:33 +0800

    增加动态属性集的功能，即可以不用定义属性变量来获取或设置属性的功能。

--- src/ComponentModel/NotifyObject.cs
+++ src/ComponentModel/NotifyObject.cs
@@ -25,6 +25,8 @@
  */
 
 using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -37,7 +39,101 @@ namespace Zongsoft.ComponentModel
 		public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
 		#endregion
 
+		#region 成员字段
+		private ConcurrentDictionary<string, object> _properties;
+		#endregion
+
+		#region 保护属性
+		protected bool HasProperties
+		{
+			get
+			{
+				return _properties != null && _properties.Count > 0;
+			}
+		}
+
+		protected IDictionary<string, object> Properties
+		{
+			get
+			{
+				if(_properties == null)
+					System.Threading.Interlocked.CompareExchange(ref _properties, new ConcurrentDictionary<string, object>(), null);
+
+				return _properties;
+			}
+		}
+		#endregion
+
 		#region 保护方法
+		protected T GetValue<T>(string propertyName, T defaultValue = default(T))
+		{
+			if(string.IsNullOrWhiteSpace(propertyName))
+				throw new ArgumentNullException("propertyName");
+
+			var properties = this.Properties;
+			object value;
+
+			if(properties.TryGetValue(propertyName.Trim(), out value))
+				return (T)value;
+
+			var property = this.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+			if(property == null)
+				throw new InvalidOperationException(string.Format("The '{0}' property is not exists.", propertyName));
+
+			//返回属性的默认值
+			return this.GetPropertyDefaultValue(property, defaultValue);
+		}
+
+		protected T GetValue<T>(Expression<Func<T>> propertyExpression, T defaultValue = default(T))
+		{
+			if(propertyExpression == null)
+				throw new ArgumentNullException("propertyExpression");
+
+			var property = this.GetPropertyInfo<T>(propertyExpression);
+
+			if(property == null)
+				throw new ArgumentException("Invalid expression of the argument", "propertyExpression");
+
+			var properties = this.Properties;
+			object value;
+
+			if(properties.TryGetValue(property.Name, out value))
+				return (T)value;
+
+			//返回属性的默认值
+			return this.GetPropertyDefaultValue(property, defaultValue);
+		}
+
+		protected void SetValue(object value, string propertyName)
+		{
+			if(string.IsNullOrWhiteSpace(propertyName))
+				throw new ArgumentNullException("propertyName");
+
+			var properties = this.Properties;
+
+			properties[propertyName.Trim()] = value;
+
+			this.OnPropertyChanged(propertyName);
+		}
+
+		protected void SetValue<T>(T value, Expression<Func<T>> propertyExpression)
+		{
+			if(propertyExpression == null)
+				throw new ArgumentNullException("propertyExpression");
+
+			var property = this.GetPropertyInfo<T>(propertyExpression);
+
+			if(property == null)
+				throw new ArgumentException("Invalid expression of the argument", "propertyExpression");
+
+			var properties = this.Properties;
+
+			properties[property.Name] = value;
+
+			this.OnPropertyChanged(property.Name);
+		}
+
 		protected void SetValue<T>(ref T target, T value, string propertyName)
 		{
 			if(object.ReferenceEquals(target, value))
@@ -51,6 +147,9 @@ namespace Zongsoft.ComponentModel
 		{
 			if(object.ReferenceEquals(target, value))
 				return;
+
+			if(propertyExpression == null)
+				throw new ArgumentNullException("propertyExpression");
 
 			var property = this.GetPropertyInfo<T>(propertyExpression);
 
@@ -89,6 +188,19 @@ namespace Zongsoft.ComponentModel
 				throw new ArgumentException("Invalid expression of the argument.", "propertyExpression");
 
 			return memberExpression.Member as PropertyInfo;
+		}
+
+		private T GetPropertyDefaultValue<T>(PropertyInfo property, T defaultValue)
+		{
+			if(property == null)
+				throw new ArgumentNullException("property");
+
+			var attribute = property.GetCustomAttribute<DefaultValueAttribute>();
+
+			if(attribute != null)
+				return Zongsoft.Common.Convert.ConvertValue<T>(attribute.Value);
+
+			return defaultValue;
 		}
 		#endregion
 	}
