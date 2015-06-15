2863b598867cd4ad6def0ea8381edc5e548b7b2d
Popeye Zhong <zongsoft@gmail.com>
2015-06-15 19:34:48 +0800

    新增 IEventHandler 接口。

---
+++ src/Common/IEventHandler.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+
+namespace Zongsoft.Common
+{
+	/// <summary>
+	/// 关于事件处理程序的接口。
+	/// </summary>
+	public interface IEventHandler
+	{
+		/// <summary>
+		/// 进行事件处理的方法。
+		/// </summary>
+		/// <param name="source">事件来源，即事件的激发者。</param>
+		/// <param name="parameter">事件参数。</param>
+		void Handle(object source, object parameter);
+	}
+}
