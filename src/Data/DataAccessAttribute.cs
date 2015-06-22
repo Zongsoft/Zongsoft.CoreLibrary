e42c654912acfcf30367aba298848ca99b1c0d67
Popeye Zhong <zongsoft@gmail.com>
2015-06-22 11:03:58 +0800

    添加了一个 DataAccessAttribute 类，可在数据访问实体类中使用。

---
+++ src/Data/DataAccessAttribute.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.ComponentModel;
+
+namespace Zongsoft.Data
+{
+	[AttributeUsage(AttributeTargets.Class)]
+	public class DataAccessAttribute : Attribute
+	{
+		#region 成员字段
+		private string _name;
+		#endregion
+
+		#region 构造函数
+		public DataAccessAttribute()
+		{
+		}
+
+		public DataAccessAttribute(string name)
+		{
+			if(string.IsNullOrWhiteSpace(name))
+				throw new ArgumentNullException("name");
+
+			_name = name.Trim();
+		}
+		#endregion
+
+		#region 公共属性
+		/// <summary>
+		/// 获取或设置数据访问的名称。
+		/// </summary>
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				if(string.IsNullOrWhiteSpace(value))
+					throw new ArgumentNullException();
+
+				_name = value.Trim();
+			}
+		}
+		#endregion
+	}
+}
