9c119ff267d07dde79a8410c2d45224ab6d58355
Popeye Zhong <zongsoft@gmail.com>
2015-06-22 13:01:15 +0800

    添加了两个单元测试类。

---
+++ tests/IO/FileSystemTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zongsoft.IO
+{
+	[TestClass]
+	public class FileSystemTest
+	{
+		public FileSystemTest()
+		{
+			if(FileSystem.Services == null)
+				FileSystem.Services = new Zongsoft.Services.ServiceProvider();
+
+			FileSystem.Services.Register(LocalFileSystem.Instance, typeof(IFileSystem));
+		}
+
+		[TestMethod]
+		public void DirectoryCreate()
+		{
+			var text = @"zfs.local:/c/temp/year/month-day/[1]123.jpg";
+			var path = Path.Parse(text);
+
+			var directoryPath = path.Schema + ":" + path.DirectoryName;
+
+			FileSystem.Directory.Create(directoryPath);
+			Assert.IsTrue(FileSystem.Directory.Exists(directoryPath));
+		}
+	}
+}
---
+++ tests/IO/PathTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zongsoft.IO
+{
+	[TestClass]
+	public class PathTest
+	{
+		[TestMethod]
+		public void PathParseTest()
+		{
+			var text = @"zfs.local:/data/images/1/year/month-day/[1]123.jpg";
+			var path = Path.Parse(text);
+
+			Assert.AreEqual("zfs.local", path.Schema);
+			Assert.AreEqual("/data/images/1/year/month-day/[1]123.jpg", path.FullPath);
+			Assert.AreEqual("/data/images/1/year/month-day/", path.DirectoryName);
+			Assert.AreEqual("[1]123.jpg", path.FileName);
+		}
+	}
+}
