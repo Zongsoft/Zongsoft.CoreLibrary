d5c9315c5a16f79e40faa1220541062c5b64adae
Popeye Zhong <zongsoft@gmail.com>
2015-07-10 17:45:53 +0800

    新增了有关消息队列的代码。

---
+++ src/Messaging/MessageBase.cs
@@ -0,0 +1,181 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Zongsoft.Messaging
+{
+	public abstract class MessageBase : MarshalByRefObject
+	{
+		#region 常量定义
+		private static readonly DateTime MINIMUM_DATETIME = new DateTime(1900, 1, 1);
+		#endregion
+
+		#region 成员字段
+		private string _id;
+		private string _acknowledgementId;
+		private byte[] _data;
+		private byte[] _checksum;
+		private DateTime _expires;
+		private DateTime _enqueuedTime;
+		private DateTime _dequeuedTime;
+		private int _dequeuedCount;
+		private byte _priority;
+		#endregion
+
+		#region 构造函数
+		protected MessageBase(string id, byte[] data, byte[] checksum = null, DateTime? expires = null, DateTime? enqueuedTime = null, DateTime? dequeuedTime = null)
+		{
+			if(string.IsNullOrWhiteSpace(id))
+				throw new ArgumentNullException("id");
+
+			_id = id.Trim();
+			_data = data;
+			_checksum = checksum;
+			_expires = (expires.HasValue ? expires.Value : DateTime.Today.AddYears(50));
+			_enqueuedTime = (enqueuedTime.HasValue ? enqueuedTime.Value : MINIMUM_DATETIME);
+			_dequeuedTime = (dequeuedTime.HasValue ? dequeuedTime.Value : MINIMUM_DATETIME);
+		}
+		#endregion
+
+		#region 公共属性
+		/// <summary>
+		/// 获取消息的编号。
+		/// </summary>
+		public string Id
+		{
+			get
+			{
+				return _id;
+			}
+		}
+
+		public string AcknowledgementId
+		{
+			get
+			{
+				return _acknowledgementId;
+			}
+			protected set
+			{
+				_acknowledgementId = value;
+			}
+		}
+
+		public byte[] Data
+		{
+			get
+			{
+				return _data;
+			}
+			set
+			{
+				_data = value;
+			}
+		}
+
+		public byte[] Checksum
+		{
+			get
+			{
+				return _checksum;
+			}
+			set
+			{
+				_checksum = value;
+			}
+		}
+
+		public DateTime Expires
+		{
+			get
+			{
+				return _expires;
+			}
+			protected set
+			{
+				_expires = value;
+			}
+		}
+
+		public DateTime EnqueuedTime
+		{
+			get
+			{
+				return _enqueuedTime;
+			}
+			protected set
+			{
+				_enqueuedTime = value;
+			}
+		}
+
+		public DateTime DequeuedTime
+		{
+			get
+			{
+				return _dequeuedTime;
+			}
+			protected set
+			{
+				_dequeuedTime = value;
+			}
+		}
+
+		public int DequeuedCount
+		{
+			get
+			{
+				return _dequeuedCount;
+			}
+			protected set
+			{
+				_dequeuedCount = value;
+			}
+		}
+
+		public byte Priority
+		{
+			get
+			{
+				return _priority;
+			}
+			protected set
+			{
+				_priority = value;
+			}
+		}
+		#endregion
+
+		#region 公共方法
+		public abstract object Acknowledge(object parameter = null);
+
+		public abstract Task<object> AcknowledgeAsync(object parameter = null);
+		#endregion
+	}
+}
---
+++ src/Messaging/MessageQueueBase.cs
@@ -0,0 +1,250 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Zongsoft.Messaging
+{
+	public abstract class MessageQueueBase : Zongsoft.Collections.IQueue<MessageBase>
+	{
+		#region 事件定义
+		public event EventHandler<Zongsoft.Collections.DequeuedEventArgs> Dequeued;
+		public event EventHandler<Zongsoft.Collections.EnqueuedEventArgs> Enqueued;
+		#endregion
+
+		#region 成员字段
+		private string _name;
+		#endregion
+
+		#region 构造函数
+		protected MessageQueueBase(string name)
+		{
+			if(string.IsNullOrWhiteSpace(name))
+				throw new ArgumentNullException("name");
+
+			_name = name.Trim();
+		}
+		#endregion
+
+		#region 公共属性
+		public virtual string Name
+		{
+			get
+			{
+				return _name;
+			}
+		}
+
+		public abstract long Count
+		{
+			get;
+		}
+		#endregion
+
+		#region 保护属性
+		protected virtual int Capacity
+		{
+			get
+			{
+				return 0;
+			}
+		}
+		#endregion
+
+		#region 队列方法
+		public virtual void Enqueue(object item)
+		{
+			this.Enqueue(new object[] { item });
+		}
+
+		public abstract void EnqueueMany<T>(IEnumerable<T> items);
+
+		public virtual MessageBase Dequeue()
+		{
+			var result = this.Dequeue(1);
+
+			if(result == null)
+				return null;
+
+			return result.FirstOrDefault();
+		}
+
+		public abstract IEnumerable<MessageBase> Dequeue(int count);
+
+		public virtual MessageBase Peek()
+		{
+			var result = this.Peek(1);
+
+			if(result == null)
+				return null;
+			else
+				return result.FirstOrDefault();
+		}
+
+		public abstract IEnumerable<MessageBase> Peek(int count);
+
+		public virtual MessageBase Take(int startOffset)
+		{
+			var result = this.Take(startOffset, 1);
+
+			if(result == null)
+				return null;
+			else
+				return result.FirstOrDefault();
+		}
+
+		public abstract IEnumerable<MessageBase> Take(int startOffset, int count);
+
+		void Zongsoft.Collections.IQueue.Clear()
+		{
+			this.ClearQueue();
+		}
+
+		object Zongsoft.Collections.IQueue.Dequeue()
+		{
+			return this.Dequeue();
+		}
+
+		IEnumerable Zongsoft.Collections.IQueue.Dequeue(int count)
+		{
+			return this.Dequeue(count);
+		}
+
+		object Zongsoft.Collections.IQueue.Peek()
+		{
+			return this.Peek();
+		}
+
+		IEnumerable Zongsoft.Collections.IQueue.Peek(int count)
+		{
+			return this.Peek(count);
+		}
+
+		object Zongsoft.Collections.IQueue.Take(int startOffset)
+		{
+			return this.TakeQueue(startOffset);
+		}
+
+		IEnumerable Zongsoft.Collections.IQueue.Take(int startOffset, int count)
+		{
+			return this.TakeQueue(startOffset, count);
+		}
+		#endregion
+
+		#region 保护方法
+		protected virtual void ClearQueue()
+		{
+			throw new NotSupportedException("The message queue does not support the operation.");
+		}
+
+		protected virtual object TakeQueue(int startOffset)
+		{
+			var result = this.TakeQueue(startOffset, 1);
+
+			if(result == null)
+				return null;
+
+			return result.GetEnumerator().Current;
+		}
+
+		protected virtual IEnumerable TakeQueue(int startOffset, int count)
+		{
+			throw new NotSupportedException("The message queue does not support the operation.");
+		}
+
+		protected virtual void CopyQueueTo(Array array, int index)
+		{
+			throw new NotSupportedException("The message queue does not support the operation.");
+		}
+		#endregion
+
+		#region 激发事件
+		protected virtual void OnDequeued(Zongsoft.Collections.DequeuedEventArgs args)
+		{
+			var handler = this.Dequeued;
+
+			if(handler != null)
+				handler(this, args);
+		}
+
+		protected virtual void OnEnqueued(Zongsoft.Collections.EnqueuedEventArgs args)
+		{
+			var handler = this.Enqueued;
+
+			if(handler != null)
+				handler(this, args);
+		}
+		#endregion
+
+		#region 显式实现
+		int Zongsoft.Collections.IQueue.Capacity
+		{
+			get
+			{
+				return this.Capacity;
+			}
+		}
+
+		int ICollection.Count
+		{
+			get
+			{
+				return (int)this.Count;
+			}
+		}
+
+		bool ICollection.IsSynchronized
+		{
+			get
+			{
+				return false;
+			}
+		}
+
+		object ICollection.SyncRoot
+		{
+			get
+			{
+				throw new NotSupportedException("The message queue does not support the operation.");
+			}
+		}
+
+		void ICollection.CopyTo(Array array, int index)
+		{
+			this.CopyQueueTo(array, index);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			throw new NotSupportedException("The message queue does not support the operation.");
+		}
+		#endregion
+	}
+}
