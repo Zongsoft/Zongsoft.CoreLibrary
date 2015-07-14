1cbab5d694f8351067fcda404b13e7400937a1a2
Popeye Zhong <zongsoft@gmail.com>
2015-07-14 16:55:50 +0800

    该类提供了将异步方法的调用转换为同步调用的工具，这在 Web 程序中尤为必要！

---
+++ src/TaskUtility.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Threading.Tasks;
+
+namespace Zongsoft
+{
+	internal static class TaskUtility
+	{
+		public static T ExecuteTask<T>(Func<Task<T>> thunk)
+		{
+			return Task.Run(() => ExecuteTaskDelegate(() => thunk())).Result;
+		}
+
+		private static async Task<T> ExecuteTaskDelegate<T>(Func<Task<T>> thunk)
+		{
+			return await thunk();
+		}
+	}
+}
