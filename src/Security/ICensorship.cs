18ea17bb680572eb181f70ba7f2250afa9d0280f
Popeye Zhong <zongsoft@gmail.com>
2015-07-25 23:28:32 +0800

    :100:  简化 ICensorship 单词审查接口。 :tractor:

--- src/Security/ICensorship.cs
+++ src/Security/ICensorship.cs
@@ -35,18 +35,11 @@ namespace Zongsoft.Security
 	public interface ICensorship
 	{
 		/// <summary>
-		/// 获取词汇审查的名称或审查的类别。
-		/// </summary>
-		string Name
-		{
-			get;
-		}
-
-		/// <summary>
 		/// 判断指定的<paramref name="word"/>词汇是否为非法的。
 		/// </summary>
 		/// <param name="word">指定要判断的单词或词语。</param>
+		/// <param name="keys">指定要判断的单词或词语所属的审查类别。</param>
 		/// <returns>如果指定的单词或词语是非法的(敏感词)则返回真(True)，否则返回假(False)。</returns>
-		bool IsBlocked(string word);
+		bool IsBlocked(string word, params string[] keys);
 	}
 }
