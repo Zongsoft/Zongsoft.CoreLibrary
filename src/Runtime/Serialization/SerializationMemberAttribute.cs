3506f94873238bad33a4ed93f919e1368ee6483b
Popeye Zhong <zongsoft@gmail.com>
2015-08-02 02:32:24 +0800

    增加一个用来控制序列化成员的特性。 :baby_chick:

---
+++ src/Runtime/Serialization/SerializationMemberAttribute.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.ComponentModel;
+
+namespace Zongsoft.Runtime.Serialization
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true)]
+	public class SerializationMemberAttribute : Attribute
+	{
+		#region 成员字段
+		private SerializationMemberBehavior _behavior;
+		#endregion
+
+		#region 构造函数
+		public SerializationMemberAttribute(SerializationMemberBehavior behavior)
+		{
+			_behavior = behavior;
+		}
+		#endregion
+
+		#region 公共属性
+		public SerializationMemberBehavior Behavior
+		{
+			get
+			{
+				return _behavior;
+			}
+			set
+			{
+				_behavior = value;
+			}
+		}
+		#endregion
+	}
+}
