869cf36147a4a4c4b1e641b146dc177348fa6731
Popeye Zhong <zongsoft@gmail.com>
2015-08-03 12:03:58 +0800

    :point_right:  增强元信息的选项值。 :pig: :mountain_bicyclist:

--- src/Runtime/Serialization/SerializationMemberAttribute.cs
+++ src/Runtime/Serialization/SerializationMemberAttribute.cs
@@ -33,10 +33,20 @@ namespace Zongsoft.Runtime.Serialization
 	public class SerializationMemberAttribute : Attribute
 	{
 		#region 成员字段
+		private string _name;
 		private SerializationMemberBehavior _behavior;
 		#endregion
 
 		#region 构造函数
+		public SerializationMemberAttribute()
+		{
+		}
+
+		public SerializationMemberAttribute(string name)
+		{
+			_name = name == null ? string.Empty : name.Trim();
+		}
+
 		public SerializationMemberAttribute(SerializationMemberBehavior behavior)
 		{
 			_behavior = behavior;
@@ -44,6 +54,24 @@ namespace Zongsoft.Runtime.Serialization
 		#endregion
 
 		#region 公共属性
+		/// <summary>
+		/// 获取或设置序列化后的成员名称，如果为空(null)或空字符串("")则取对应的成员本身的名称。
+		/// </summary>
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				_name = value == null ? string.Empty : value.Trim();
+			}
+		}
+
+		/// <summary>
+		/// 获取或设置成员的序列化行为。
+		/// </summary>
 		public SerializationMemberBehavior Behavior
 		{
 			get
--- src/Runtime/Serialization/SerializationMemberBehavior.cs
+++ src/Runtime/Serialization/SerializationMemberBehavior.cs
@@ -31,7 +31,13 @@ namespace Zongsoft.Runtime.Serialization
 {
 	public enum SerializationMemberBehavior
 	{
+		/// <summary>未定义，默认状态。</summary>
+		None,
+
+		/// <summary>忽略序列化对应的成员。</summary>
 		Ignored,
+
+		/// <summary>必须序列化对应的成员。</summary>
 		Required,
 	}
 }
