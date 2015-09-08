fcf9a32cf34e328457435b687a87746c68cee4f0
Popeye Zhong <zongsoft@gmail.com>
2015-09-08 15:15:51 +0800

    :sunflower:  修复文本日志记录器的一个非法关闭基类文件流的错误。 :strawberry:

--- src/Diagnostics/TextFileLogger.cs
+++ src/Diagnostics/TextFileLogger.cs
@@ -35,7 +35,8 @@ namespace Zongsoft.Diagnostics
 		#region 重写方法
 		protected override void WriteLog(LogEntry entry, Stream stream)
 		{
-			using(var writer = new StreamWriter(stream, System.Text.Encoding.UTF8))
+			//注意：此处不能关闭stream参数传入的流，该流由基类确保安全释放！
+			using(var writer = new StreamWriter(stream, System.Text.Encoding.UTF8, 1024 * 16, true))
 			{
 				writer.WriteLine(entry.ToString());
 			}
