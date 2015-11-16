b37c8f2ff0f6735215ea5232a1d09be1e6a201a9
Popeye Zhong <zongsoft@gmail.com>
2015-11-16 12:35:11 +0800

    :+1: 为服务模型添加了一个 ServiceBase 业务服务的基类，以及一个依赖注入的Attribute类。 :athletic_shoe:

---
+++ src/Services/ServiceAttribute.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.ComponentModel;
+
+namespace Zongsoft.Services
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true)]
+	public class ServiceAttribute : Attribute
+	{
+		#region 成员字段
+		private string _name;
+		private Type _contract;
+		#endregion
+
+		#region 构造函数
+		public ServiceAttribute()
+		{
+		}
+
+		public ServiceAttribute(string name)
+		{
+			this.Name = name;
+		}
+
+		public ServiceAttribute(Type contract)
+		{
+			this.Contract = contract;
+		}
+		#endregion
+
+		#region 公共属性
+		/// <summary>
+		/// 获取或设置服务的名称。
+		/// </summary>
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				_name = string.IsNullOrWhiteSpace(value) ? null : value.Trim();
+			}
+		}
+
+		/// <summary>
+		/// 获取或设置服务的契约类型。
+		/// </summary>
+		public Type Contract
+		{
+			get
+			{
+				return _contract;
+			}
+			set
+			{
+				_contract = value;
+			}
+		}
+		#endregion
+	}
+}
---
+++ src/Services/ServiceBase.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+
+namespace Zongsoft.Services
+{
+	public abstract class ServiceBase : MarshalByRefObject
+	{
+		#region 成员字段
+		private IServiceProvider _serviceProvider;
+		#endregion
+
+		#region 构造函数
+		protected ServiceBase(IServiceProvider serviceProvider)
+		{
+			if(serviceProvider == null)
+				throw new ArgumentNullException("serviceProvider");
+
+			_serviceProvider = serviceProvider;
+		}
+		#endregion
+
+		#region 公共属性
+		protected virtual IServiceProvider ServiceProvider
+		{
+			get
+			{
+				return _serviceProvider;
+			}
+			set
+			{
+				if(value == null)
+					throw new ArgumentNullException();
+
+				_serviceProvider = value;
+			}
+		}
+		#endregion
+
+		#region 获取方法
+		protected virtual object EnsureService(string name)
+		{
+			return ServiceProviderExtension.EnsureResolve(this.EnsureProvider(), name);
+		}
+
+		protected virtual object EnsureService(Type type)
+		{
+			return ServiceProviderExtension.EnsureResolve(this.EnsureProvider(), type);
+		}
+
+		protected virtual object EnsureService(Type type, object parameter)
+		{
+			return ServiceProviderExtension.EnsureResolve(this.EnsureProvider(), type, parameter);
+		}
+
+		protected virtual T EnsureService<T>() where T : class
+		{
+			return ServiceProviderExtension.EnsureResolve<T>(this.EnsureProvider());
+		}
+
+		protected virtual T EnsureService<T>(object parameter) where T : class
+		{
+			return ServiceProviderExtension.EnsureResolve<T>(this.EnsureProvider(), parameter);
+		}
+		#endregion
+
+		#region 私有方法
+		private IServiceProvider EnsureProvider()
+		{
+			var provider = this.ServiceProvider;
+
+			if(provider == null)
+				throw new InvalidOperationException("Missing service provider.");
+
+			return provider;
+		}
+		#endregion
+	}
+}
