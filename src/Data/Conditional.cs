a1c7569293323a87a75348ba43636a6dbdf520ab
Popeye Zhong <zongsoft@gmail.com>
2016-03-07 11:39:09 +0800

    :congratulations:  完善条件查询接口。 :+1:

--- src/Data/Conditional.cs
+++ src/Data/Conditional.cs
@@ -25,14 +25,48 @@
  */
 
 using System;
+using System.Collections.Generic;
 
 namespace Zongsoft.Data
 {
-	/// <summary>
-	/// 表示条件设置实体的接口。
-	/// </summary>
-	public interface IConditional : ICondition
+	public abstract class Conditional : Zongsoft.ComponentModel.NotifyObject, IConditional
 	{
-		ConditionCollection ToConditions();
+		#region 成员字段
+		private ConditionCombine _conditionCombine;
+		#endregion
+
+		#region 构造函数
+		protected Conditional()
+		{
+		}
+		#endregion
+
+		#region 保护属性
+		protected ConditionCombine ConditionCombine
+		{
+			get
+			{
+				return _conditionCombine;
+			}
+			set
+			{
+				_conditionCombine = value;
+			}
+		}
+		#endregion
+
+		#region 符号重写
+		public static implicit operator ConditionCollection(Conditional conditional)
+		{
+			if(conditional == null)
+				return null;
+
+			return conditional.ToConditions();
+		}
+		#endregion
+
+		#region 公共方法
+		public abstract ConditionCollection ToConditions();
+		#endregion
 	}
 }
