fee835636e456eeba7e73330dd3768ba82f64d13
Popeye Zhong <zongsoft@gmail.com>
2016-06-11 17:02:54 +0800

    为验证失败原因枚举增加一个失败项。 :fallen_leaf:

--- src/Security/Membership/AuthenticationReason.cs
+++ src/Security/Membership/AuthenticationReason.cs
@@ -35,23 +35,26 @@ namespace Zongsoft.Security.Membership
 	public enum AuthenticationReason
 	{
 		/// <summary>验证成功</summary>
-		[Description("${Text.AuthenticationReason.Succeed.Description}")]
+		[Description("${Text.AuthenticationReason.Succeed}")]
 		Succeed = 0,
 		/// <summary>未知的原因</summary>
-		[Description("${Text.AuthenticationReason.Unknown.Description}")]
+		[Description("${Text.AuthenticationReason.Unknown}")]
 		Unknown = -1,
 
 		/// <summary>无效的身份标识</summary>
-		[Description("${Text.AuthenticationReason.InvalidIdentity.Description}")]
+		[Description("${Text.AuthenticationReason.InvalidIdentity}")]
 		InvalidIdentity = 1,
 		/// <summary>无效的密码</summary>
-		[Description("${Text.AuthenticationReason.InvalidPassword.Description}")]
+		[Description("${Text.AuthenticationReason.InvalidPassword}")]
 		InvalidPassword = 2,
 		/// <summary>帐户尚未批准</summary>
-		[Description("${Text.AuthenticationReason.AccountUnapproved.Description}")]
+		[Description("${Text.AuthenticationReason.AccountUnapproved}")]
 		AccountUnapproved = 3,
-		/// <summary>帐户已被禁用</summary>
-		[Description("${Text.AuthenticationReason.AccountSuspended.Description}")]
+		/// <summary>帐户被暂时挂起（可能是因为密码验证失败次数过多。）</summary>
+		[Description("${Text.AuthenticationReason.AccountSuspended}")]
 		AccountSuspended = 4,
+		/// <summary>帐户已被禁用</summary>
+		[Description("${Text.AuthenticationReason.AccountDisabled}")]
+		AccountDisabled = 5,
 	}
 }
