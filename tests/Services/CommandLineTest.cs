5db1230e1eb43ff478c2b2aaaa65175d0a411507
Popeye Zhong <zongsoft@gmail.com>
2016-11-04 15:43:25 +0800

    添加一个命令行解析的单元测试项。 :tangerine:

---
+++ tests/Services/CommandLineTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xunit;
+
+namespace Zongsoft.Services.Tests
+{
+	public class CommandLineTest
+	{
+		[Fact]
+		public void TestCommandLineParse()
+		{
+			Assert.Null(CommandLine.Parse(""));
+
+			CommandLine line = null;
+
+			//line = CommandLine.Parse("/");
+			//Assert.NotNull(line);
+			//Assert.Equal("/", line.Name);
+			//Assert.Equal("", line.Path);
+			//Assert.Equal("/", line.FullPath);
+
+			//line = CommandLine.Parse(".");
+			//Assert.NotNull(line);
+			//Assert.Equal(".", line.Name);
+			//Assert.Equal("", line.Path);
+			//Assert.Equal(".", line.FullPath);
+
+			//line = CommandLine.Parse("..");
+			//Assert.NotNull(line);
+			//Assert.Equal("..", line.Name);
+			//Assert.Equal("", line.Path);
+			//Assert.Equal("..", line.FullPath);
+
+			line = CommandLine.Parse("send");
+			Assert.Equal("send", line.Name);
+			Assert.Equal("", line.Path);
+			Assert.Equal("send", line.FullPath);
+
+			line = CommandLine.Parse("/send");
+			Assert.Equal("send", line.Name);
+			Assert.Equal("/", line.Path);
+			Assert.Equal("/send", line.FullPath);
+
+			line = CommandLine.Parse("./send");
+			Assert.Equal("send", line.Name);
+			Assert.Equal("", line.Path);
+			Assert.Equal("send", line.FullPath);
+
+			line = CommandLine.Parse("../send");
+			Assert.Equal("send", line.Name);
+			Assert.Equal("..", line.Path);
+			Assert.Equal("../send", line.FullPath);
+
+			line = CommandLine.Parse("sms.send");
+			Assert.Equal("send", line.Name);
+			Assert.Equal("sms", line.Path);
+			Assert.Equal("sms/send", line.FullPath);
+
+			line = CommandLine.Parse("/sms.send");
+			Assert.Equal("send", line.Name);
+			Assert.Equal("/sms", line.Path);
+			Assert.Equal("/sms/send", line.FullPath);
+
+			line = CommandLine.Parse("./sms.send");
+			Assert.Equal("send", line.Name);
+			Assert.Equal("sms", line.Path);
+			Assert.Equal("sms/send", line.FullPath);
+
+			line = CommandLine.Parse("../sms.send");
+			Assert.Equal("send", line.Name);
+			Assert.Equal("../sms", line.Path);
+			Assert.Equal("../sms/send", line.FullPath);
+
+		}
+	}
+}
