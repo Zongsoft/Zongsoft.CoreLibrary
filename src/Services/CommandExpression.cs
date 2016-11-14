0ab9fe044778e3274bbb1c93561c347de5c95d3a
Popeye Zhong <zongsoft@gmail.com>
2016-11-15 00:18:54 +0800

    feat: 准备重构命令表达式相关的内容。 :neckbeard: :fire:

---
+++ src/Services/CommandExpression.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2011-2016 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Zongsoft.Services
+{
+	public class CommandExpression
+	{
+		#region 成员字段
+		private string _name;
+		private string _path;
+		private string _fullPath;
+		private Zongsoft.IO.PathAnchor _anchor;
+		private IDictionary<string, string> _options;
+		private ICollection<string> _arguments;
+		private CommandExpression _next;
+		#endregion
+
+		#region 构造函数
+		public CommandExpression(string fullPath, IDictionary<string, string> options = null, params string[] arguments)
+		{
+			if(string.IsNullOrWhiteSpace(fullPath))
+				throw new ArgumentNullException("fullPath");
+
+			var parts = fullPath.Split('/', '.');
+
+			for(int i = parts.Length - 1; i >= 0; i--)
+			{
+				if(string.IsNullOrWhiteSpace(parts[i]))
+					continue;
+
+				if(_name == null)
+					_name = parts[i].Trim();
+				else
+				{
+					if(_path == null)
+						_path = parts[i].Trim();
+					else
+						_path = parts[i].Trim() + "/" + _path;
+				}
+			}
+
+			if(_name == null)
+				_name = "/";
+			else
+				_path = "/" + _path;
+
+			if(_name == "/")
+				_fullPath = "/";
+			else
+			{
+				if(_path == "/")
+					_fullPath = _path + _name;
+				else
+					_fullPath = _path + "/" + _name;
+			}
+
+			if(_options == null)
+			{
+				if(options == null)
+					_options = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+				else
+					_options = new Dictionary<string, string>(options, StringComparer.OrdinalIgnoreCase);
+			}
+			else
+			{
+				if(options != null && options.Count > 0)
+				{
+					foreach(var option in options)
+						_options[option.Key] = option.Value;
+				}
+			}
+
+			_arguments = arguments;
+		}
+
+		private CommandExpression(string[] parts, string prefix)
+		{
+			if(parts == null)
+				throw new ArgumentNullException("parts");
+
+			_name = string.Empty;
+			_path = string.Empty;
+
+			for(int i = parts.Length - 1; i >= 0; i--)
+			{
+				if(string.IsNullOrWhiteSpace(parts[i]))
+					continue;
+
+				if(string.IsNullOrEmpty(_name))
+					_name = parts[i].Trim();
+				else
+				{
+					if(string.IsNullOrEmpty(_path))
+						_path = parts[i].Trim();
+					else
+						_path = parts[i].Trim() + "/" + _path;
+				}
+			}
+
+			if(prefix == "..")
+				_path = ".." + (string.IsNullOrEmpty(_path) ? "" : "/") + _path;
+			else if(prefix == "/")
+				_path = "/" + _path;
+
+			if(string.IsNullOrWhiteSpace(_path))
+				_fullPath = _name;
+			else
+				_fullPath = _path.TrimEnd('/') + "/" + _name;
+
+			_options = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+		}
+		#endregion
+
+		#region 公共属性
+		public Zongsoft.IO.PathAnchor Anchor
+		{
+			get
+			{
+				return _anchor;
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+		}
+
+		public string Path
+		{
+			get
+			{
+				return _path;
+			}
+		}
+
+		public string FullPath
+		{
+			get
+			{
+				return _fullPath;
+			}
+		}
+
+		public IDictionary<string, string> Options
+		{
+			get
+			{
+				return _options;
+			}
+		}
+
+		public ICollection<string> Arguments
+		{
+			get
+			{
+				if(_arguments == null)
+					System.Threading.Interlocked.CompareExchange(ref _arguments, new List<string>(), null);
+
+				return _arguments;
+			}
+		}
+
+		public CommandExpression Next
+		{
+			get
+			{
+				return _next;
+			}
+			set
+			{
+				_next = value;
+			}
+		}
+		#endregion
+	}
+}
---
+++ src/Services/CommandExpressionParser.cs
@@ -0,0 +1,372 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2011-2016 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zongsoft.Services
+{
+	public class CommandExpressionParser : ICommandExpressionParser
+	{
+		#region 私有枚举
+		private enum CommandPathState
+		{
+			None,
+			Dot,
+			DoubleDot,
+			Slash,
+			Character,
+		}
+
+		private enum CommandExpressionState
+		{
+			None,
+			Dot,
+			DoubleDot,
+			Slash,
+			Character,
+			Whitespace,
+			Pipe,
+			Option,
+			OptionKey,
+			OptionValue,
+			OptionDelimiter,
+		}
+		#endregion
+
+		#region 单例字段
+		public static readonly ICommandExpressionParser Default = new CommandExpressionParser();
+		#endregion
+
+		#region 解析方法
+		public CommandExpression Parse(string text)
+		{
+			if(string.IsNullOrWhiteSpace(text))
+				throw new ArgumentNullException(nameof(text));
+
+			var name = string.Empty;
+			var value = string.Empty;
+			var state = CommandExpressionState.None;
+			CommandExpression expression = null;
+
+			using(var reader = new StringReader(text))
+			{
+				var path = ParsePath(reader);
+				char chr;
+
+				while((chr = (char)reader.Read()) > 0)
+				{
+					if(chr == '-' || chr == '/')
+					{
+						if(state == CommandExpressionState.None)
+							state = CommandExpressionState.OptionKey;
+						else
+							throw new CommandExpressionException("");
+					}
+					else if(chr == '=' || chr == ':')
+					{
+						if(state == CommandExpressionState.OptionKey)
+							state = CommandExpressionState.OptionDelimiter;
+						else
+							value += chr;
+					}
+					else if(chr == '"' || chr == '\'')
+					{
+						ParseString(reader, chr);
+					}
+					else if(Char.IsLetterOrDigit(chr) || chr == '_')
+					{
+						if(state == CommandExpressionState.OptionKey)
+							name += chr;
+						else if(state == CommandExpressionState.OptionValue)
+							value += chr;
+					}
+					else if(Char.IsWhiteSpace(chr))
+					{
+						if(string.IsNullOrEmpty(name))
+							expression.Arguments.Add(value);
+						else
+							expression.Options.Add(name, value);
+
+						state = CommandExpressionState.None;
+					}
+				}
+			}
+
+			throw new NotImplementedException();
+		}
+		#endregion
+
+		private CommandPathDesciption ParsePath(TextReader reader)
+		{
+			if(reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			var state = CommandPathState.None;
+			var path = new CommandPathDesciption();
+
+			char chr;
+
+			while((chr = (char)reader.Read()) > 0)
+			{
+				if(chr == '.')
+				{
+					switch(state)
+					{
+						case CommandPathState.None:
+							state = CommandPathState.Dot;
+							path.Anchor = IO.PathAnchor.Current;
+							break;
+						case CommandPathState.Dot:
+							state = CommandPathState.DoubleDot;
+							path.Anchor = IO.PathAnchor.Parent;
+							break;
+						case CommandPathState.Character:
+							state = CommandPathState.Slash;
+							chr = '/';
+							break;
+						default:
+							throw new CommandExpressionException("Invalid command expression.");
+					}
+				}
+				else if(chr == '/')
+				{
+					if(state == CommandPathState.Slash)
+						throw new CommandExpressionException("Invalid command expression.");
+
+					state = CommandPathState.Slash;
+				}
+				else if(Char.IsLetterOrDigit(chr) || chr == '_')
+				{
+					if(state == CommandPathState.Dot || state == CommandPathState.DoubleDot)
+						throw new CommandExpressionException("Invalid command expression.");
+
+					state = CommandPathState.Character;
+				}
+				else if(Char.IsWhiteSpace(chr))
+				{
+					if(state == CommandPathState.Character)
+						return path;
+				}
+
+				if(!Char.IsWhiteSpace(chr))
+					path.FullPath += chr;
+			}
+
+			return path;
+		}
+
+		private void ParsePath(string text, int index)
+		{
+			var state = CommandPathState.None;
+			var path = string.Empty;
+
+			while(index < text.Length)
+			{
+				var chr = text[index];
+
+				if(chr == '.')
+				{
+					switch(state)
+					{
+						case CommandPathState.None:
+							state = CommandPathState.Dot;
+							break;
+						case CommandPathState.Dot:
+							state = CommandPathState.DoubleDot;
+							break;
+						case CommandPathState.Character:
+							state = CommandPathState.Slash;
+							chr = '/';
+							break;
+						default:
+							throw new CommandExpressionException("Invalid command expression.");
+					}
+				}
+				else if(chr == '/')
+				{
+					if(state == CommandPathState.Slash)
+						throw new CommandExpressionException("Invalid command expression.");
+
+					state = CommandPathState.Slash;
+				}
+				else if(Char.IsLetterOrDigit(chr) || chr == '_')
+				{
+					if(state == CommandPathState.Dot || state == CommandPathState.DoubleDot)
+						throw new CommandExpressionException("Invalid command expression.");
+
+					state = CommandPathState.Character;
+				}
+
+				if(!Char.IsWhiteSpace(chr))
+					path += chr;
+			}
+		}
+
+		private static string ParseString(string text, int index = 0, char quote = '"')
+		{
+			return ParseString(text, index, -1, quote);
+		}
+
+		private static string ParseString(string text, int index, int count, char quote)
+		{
+			if(string.IsNullOrEmpty(text))
+				return string.Empty;
+
+			if(index < 0)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			if(index >= text.Length)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			if(count == 0)
+				throw new ArgumentOutOfRangeException(nameof(count));
+
+			if(count > 0 && count > text.Length - index)
+				throw new ArgumentOutOfRangeException(nameof(count));
+
+			var isEscaping = false;
+			var result = string.Empty;
+
+			if(count < 0)
+				count = text.Length - index;
+
+			for(int i = 0; i < count; i++)
+			{
+				var chr = text[index + i];
+
+				if(isEscaping)
+				{
+					if(chr == 't')
+						chr = '\t';
+					else if(chr != quote && chr != '\\')
+						result += '\\';
+				}
+				else
+				{
+					if(chr == quote)
+						return result;
+				}
+
+				//设置转义状态：即当前字符为转义符并且当前状态不为转义状态
+				isEscaping = chr == '\\' && (!isEscaping);
+
+				if(isEscaping)
+					continue;
+
+				result += chr;
+			}
+
+			return result;
+		}
+
+		private static string ParseString(StringReader reader, char quote = '"')
+		{
+			if(reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			int value;
+			var isEscaping = false;
+			var result = string.Empty;
+
+			while((value = reader.Read()) > 0)
+			{
+				var chr = (char)value;
+
+				if(isEscaping)
+				{
+					if(chr == 't')
+						chr = '\t';
+					else if(chr != quote && chr != '\\')
+						result += '\\';
+				}
+				else
+				{
+					if(chr == quote)
+						return result;
+				}
+
+				//设置转义状态：即当前字符为转义符并且当前状态不为转义状态
+				isEscaping = chr == '\\' && (!isEscaping);
+
+				if(isEscaping)
+					continue;
+
+				result += chr;
+			}
+
+			return result;
+		}
+
+		private static string ParseStringNew(StringReader reader, char quote = '"')
+		{
+			if(reader == null)
+				throw new ArgumentNullException(nameof(reader));
+
+			int value;
+			var isEscaping = false;
+			var result = string.Empty;
+
+			while((value = reader.Read()) > 0)
+			{
+				var chr = (char)value;
+
+				//设置转义状态：即当前字符为转义符并且当前状态不为转义状态
+				isEscaping = chr == '\\' && (!isEscaping);
+
+				if(isEscaping)
+				{
+					if(chr == 't')
+						chr = '\t';
+					else if(chr != quote && chr != '\\')
+						result += '\\';
+				}
+				else
+				{
+					if(chr == quote)
+						return result;
+				}
+
+				if(isEscaping)
+					continue;
+
+				result += chr;
+			}
+
+			return result;
+		}
+
+		private struct CommandPathDesciption
+		{
+			public Zongsoft.IO.PathAnchor Anchor;
+			public string FullPath;
+			public string Name;
+		}
+	}
+}
