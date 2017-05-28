4e68a36be04d8993d9dc24f6c8e92cf09553987a
Popeye Zhong <9555843@qq.com>
2017-05-28 15:37:27 +0800

    style: 调整代码样式。 :rabbit:

--- src/ComponentModel/EncodingConverter.cs
+++ src/ComponentModel/EncodingConverter.cs
@@ -25,14 +25,14 @@
  */
 
 using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Zongsoft.ComponentModel
 {
 	public class EncodingConverter : System.ComponentModel.TypeConverter
 	{
-		public override bool CanConvertFrom(System.ComponentModel.ITypeDescriptorContext context, Type sourceType)
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
 		{
 			if(sourceType.IsPrimitive || sourceType == typeof(string) || sourceType == typeof(decimal))
 				return true;
@@ -40,7 +40,7 @@ namespace Zongsoft.ComponentModel
 			return base.CanConvertFrom(context, sourceType);
 		}
 
-		public override bool CanConvertTo(System.ComponentModel.ITypeDescriptorContext context, Type destinationType)
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
 		{
 			if(destinationType.IsPrimitive || destinationType == typeof(string) || destinationType == typeof(decimal))
 				return true;
@@ -48,7 +48,7 @@ namespace Zongsoft.ComponentModel
 			return base.CanConvertTo(context, destinationType);
 		}
 
-		public override object ConvertFrom(System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
+		public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
 		{
 			if(value == null)
 				return Encoding.UTF8;
@@ -84,7 +84,7 @@ namespace Zongsoft.ComponentModel
 			return base.ConvertFrom(context, culture, value);
 		}
 
-		public override object ConvertTo(System.ComponentModel.ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
+		public override object ConvertTo(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value, Type destinationType)
 		{
 			var encoding = value as Encoding;
 
