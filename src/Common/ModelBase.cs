a8268ea15b32bacbb634afc8c50ac7cd0edd147a
Popeye Zhong <9555843@qq.com>
2017-06-17 17:30:09 +0800

    feat: 增强设置属性方法的功能，并重新实现部分设置逻辑。 :zap: :boar:

--- src/Common/ModelBase.cs
+++ src/Common/ModelBase.cs
@@ -97,7 +97,7 @@ namespace Zongsoft.Common
 			PropertyToken token;
 			var properties = _properties;
 
-			if(properties != null && properties.TryGetValue(propertyName.Trim(), out token))
+			if(properties != null && properties.TryGetValue(propertyName, out token))
 				return (T)token.Value;
 
 			var property = this.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
@@ -119,7 +119,7 @@ namespace Zongsoft.Common
 			var properties = _properties;
 			PropertyToken token;
 
-			if(properties != null && properties.TryGetValue(propertyName.Trim(), out token))
+			if(properties != null && properties.TryGetValue(propertyName, out token))
 				return (T)token.Value;
 
 			var value = valueFactory();
@@ -132,7 +132,7 @@ namespace Zongsoft.Common
 			if(propertyExpression == null)
 				throw new ArgumentNullException(nameof(propertyExpression));
 
-			var property = this.GetPropertyInfo<T>(propertyExpression);
+			var property = this.GetPropertyInfo(propertyExpression);
 			var properties = _properties;
 			PropertyToken token;
 
@@ -150,7 +150,7 @@ namespace Zongsoft.Common
 			if(valueFactory == null)
 				throw new ArgumentNullException(nameof(valueFactory));
 
-			var property = this.GetPropertyInfo<T>(propertyExpression);
+			var property = this.GetPropertyInfo(propertyExpression);
 			var properties = _properties;
 			PropertyToken token;
 
@@ -160,51 +160,6 @@ namespace Zongsoft.Common
 			var value = valueFactory();
 			this.SetPropertyValueCore(property, value);
 			return value;
-		}
-
-		protected void SetPropertyValue(string propertyName, object value)
-		{
-			if(string.IsNullOrWhiteSpace(propertyName))
-				throw new ArgumentNullException(nameof(propertyName));
-
-			var changed = true;
-			var properties = this.Properties;
-			PropertyToken token;
-
-			//如果指定的属性没有被缓存
-			if(!properties.TryGetValue(propertyName, out token))
-			{
-				var property = this.GetType().GetProperty(propertyName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-
-				if(property == null)
-					throw new ArgumentException($"The '{propertyName}' property is not existed.");
-
-				token = this.CreatePropertyToken(property.Name, property.PropertyType, null);
-			}
-
-			//调用属性设置通知
-			token.Value = this.OnPropertySet(propertyName, token.Type, token.Value, value);
-
-			//设置属性值
-			properties.AddOrUpdate(propertyName, token, (_, original) => {
-				changed = !object.Equals(token.Value, original.Value);
-				return token;
-			});
-
-			if(changed)
-				this.RaisePropertyChanged(propertyName, token.Value);
-		}
-
-		protected void SetPropertyValue<T>(Expression<Func<T>> propertyExpression, T value)
-		{
-			if(propertyExpression == null)
-				throw new ArgumentNullException(nameof(propertyExpression));
-
-			//解析属性表达式
-			var property = this.GetPropertyInfo<T>(propertyExpression);
-
-			//设置属性值
-			this.SetPropertyValueCore(property, value);
 		}
 
 		protected void SetPropertyValue<T>(string propertyName, ref T target, T value)
@@ -225,7 +180,7 @@ namespace Zongsoft.Common
 				throw new ArgumentNullException(nameof(propertyExpression));
 
 			//获取属性表达式指定的属性信息
-			var property = this.GetPropertyInfo<T>(propertyExpression);
+			var property = this.GetPropertyInfo(propertyExpression);
 
 			//更新目标的值
 			target = value;
@@ -234,26 +189,106 @@ namespace Zongsoft.Common
 			this.RaisePropertyChanged(property.Name, value);
 		}
 
-		private void SetPropertyValueCore(PropertyInfo property, object value)
+		protected void SetPropertyValue(string propertyName, object value, Action<object, object> onChanged = null)
+		{
+			if(string.IsNullOrWhiteSpace(propertyName))
+				throw new ArgumentNullException(nameof(propertyName));
+
+			var changed = true;
+			var properties = this.Properties;
+			object originalValue = null;
+
+			properties.AddOrUpdate(propertyName,
+				key =>
+				{
+					var property = this.GetType().GetProperty(key, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+					if(property == null)
+						throw new ArgumentException($"The '{key}' property is not existed.");
+
+					//获取属性的默认值，注意：不能使用反射获取属性值，因为属性的获取器中可能会调用该方法而导致死循环
+					originalValue = this.GetPropertyDefaultValue(property);
+
+					var propertyValue = this.OnPropertySet(property.Name, property.PropertyType, originalValue, value);
+					return this.CreatePropertyToken(property.Name, property.PropertyType, propertyValue);
+				}, (_, original) =>
+				{
+					originalValue = original.Value;
+					var propertyValue = this.OnPropertySet(original.Name, original.Type, original.Value, value);
+					changed = !object.Equals(propertyValue, original.Value);
+
+					if(changed)
+						return original.Clone(propertyValue);
+					else
+						return original;
+				});
+
+			if(changed)
+			{
+				var newValue = properties[propertyName].Value;
+
+				if(onChanged != null)
+					onChanged(originalValue, newValue);
+
+				//激发“PropertyChanged”事件，并将当前属性加入到更改集中
+				this.RaisePropertyChanged(propertyName, newValue);
+			}
+		}
+
+		protected void SetPropertyValue<T>(Expression<Func<T>> propertyExpression, T value, Action<T, T> onChanged = null)
+		{
+			if(propertyExpression == null)
+				throw new ArgumentNullException(nameof(propertyExpression));
+
+			//解析属性表达式
+			var property = this.GetPropertyInfo(propertyExpression);
+
+			//设置属性值
+			if(onChanged == null)
+				this.SetPropertyValueCore(property, value);
+			else
+				this.SetPropertyValueCore(property, value, (oldValue, newValue) => onChanged((T)oldValue, (T)newValue));
+		}
+
+		private void SetPropertyValueCore(PropertyInfo property, object value, Action<object, object> onChanged = null)
 		{
 			if(property == null)
 				throw new ArgumentNullException(nameof(property));
 
-			PropertyToken token;
+			var changed = true;
 			var properties = this.Properties;
+			object originalValue = null;
 
-			//判断指定的属性是否已被缓存
-			if(!properties.TryGetValue(property.Name, out token))
+			properties.AddOrUpdate(property.Name,
+				key =>
+				{
+					//获取属性的默认值，注意：不能使用反射获取属性值，因为属性的获取器中可能会调用该方法而导致死循环
+					originalValue = this.GetPropertyDefaultValue(property);
+
+					var propertyValue = this.OnPropertySet(property.Name, property.PropertyType, originalValue, value);
+					return this.CreatePropertyToken(property.Name, property.PropertyType, propertyValue);
+				}, (_, original) =>
+				{
+					originalValue = original.Value;
+					var propertyValue = this.OnPropertySet(original.Name, original.Type, original.Value, value);
+					changed = !object.Equals(propertyValue, original.Value);
+
+					if(changed)
+						return original.Clone(propertyValue);
+					else
+						return original;
+				});
+
+			if(changed)
 			{
-				token = this.CreatePropertyToken(property.Name, property.PropertyType, null);
-				properties.TryAdd(property.Name, token);
+				var newValue = properties[property.Name].Value;
+
+				if(onChanged != null)
+					onChanged(originalValue, newValue);
+
+				//激发“PropertyChanged”事件，并将当前属性加入到更改集中
+				this.RaisePropertyChanged(property.Name, newValue);
 			}
-
-			//调用属性设置通知
-			token.Value = this.OnPropertySet(property.Name, token.Type, token.Value, value);
-
-			//激发“PropertyChanged”事件，并将当前属性加入到更改集中
-			this.RaisePropertyChanged(property.Name, token.Value);
 		}
 		#endregion
 
@@ -282,7 +317,7 @@ namespace Zongsoft.Common
 		#region 虚拟方法
 		protected virtual object OnPropertySet(string name, Type type, object oldValue, object newValue)
 		{
-			return newValue;
+			return Zongsoft.Common.Convert.ConvertValue(newValue, type);
 		}
 
 		protected virtual PropertyToken CreatePropertyToken(string name, Type type, object value)
@@ -338,6 +373,19 @@ namespace Zongsoft.Common
 			return (PropertyInfo)memberExpression.Member;
 		}
 
+		private object GetPropertyDefaultValue(PropertyInfo property)
+		{
+			if(property == null)
+				throw new ArgumentNullException(nameof(property));
+
+			var attribute = property.GetCustomAttribute<DefaultValueAttribute>();
+
+			if(attribute != null)
+				return Zongsoft.Common.Convert.ConvertValue(attribute.Value, property.PropertyType);
+
+			return Zongsoft.Common.TypeExtension.GetDefaultValue(property.PropertyType);
+		}
+
 		private T GetPropertyDefaultValue<T>(PropertyInfo property, T defaultValue)
 		{
 			if(property == null)
@@ -346,19 +394,22 @@ namespace Zongsoft.Common
 			var attribute = property.GetCustomAttribute<DefaultValueAttribute>();
 
 			if(attribute != null)
-				return Zongsoft.Common.Convert.ConvertValue<T>(attribute.Value);
+				return Zongsoft.Common.Convert.ConvertValue<T>(attribute.Value, defaultValue);
 
 			return defaultValue;
 		}
 		#endregion
 
 		#region 嵌套子类
-		public class PropertyToken
+		public struct PropertyToken
 		{
-			public string Name;
-			public object Value;
+			#region 成员字段
+			public readonly string Name;
 			public readonly Type Type;
+			public object Value;
+			#endregion
 
+			#region 构造函数
 			public PropertyToken(string name, Type type, object value)
 			{
 				if(string.IsNullOrWhiteSpace((name)))
@@ -370,6 +421,21 @@ namespace Zongsoft.Common
 				this.Type = type;
 				this.Value = value;
 			}
+			#endregion
+
+			#region 公共方法
+			public PropertyToken Clone(object value)
+			{
+				return new PropertyToken(this.Name, this.Type, value);
+			}
+			#endregion
+
+			#region 重写方法
+			public override string ToString()
+			{
+				return string.Format("{0}({1})={2}", this.Name, this.Type.Name, this.Value);
+			}
+			#endregion
 		}
 		#endregion
 	}
