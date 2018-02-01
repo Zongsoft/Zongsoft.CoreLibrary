8c807d083baf6712c1a290d6257d4f3a5b388aa6
Popeye Zhong <9555843@qq.com>
2018-02-01 19:27:43 +0800

    feat: 增加 INotifier 接口及其他。 :no_bell:

---
+++ src/Communication/INotifier.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015-2018 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Threading.Tasks;
+
+namespace Zongsoft.Communication
+{
+	/// <summary>
+	/// 表示通知激发器的接口。
+	/// </summary>
+	public interface INotifier
+	{
+		/// <summary>
+		/// 激发一个通知给指定的接受者。
+		/// </summary>
+		/// <param name="name">指定要激发的通知名。</param>
+		/// <param name="content">指定的通知内容。</param>
+		/// <param name="destination">指定通知的接受者，由具体实现者定义支持的接受者类型。</param>
+		/// <param name="settings">指定的通知设置。</param>
+		/// <returns>返回通知结果对象，由具体实现者定义。</returns>
+		object Notify(string name, object content, object destination, object settings = null);
+
+		/// <summary>
+		/// 异步激发一个通知给指定的接受者。
+		/// </summary>
+		/// <param name="name">指定要激发的通知名。</param>
+		/// <param name="content">指定的通知内容。</param>
+		/// <param name="destination">指定通知的接受者，由具体实现者定义支持的接受者类型。</param>
+		/// <param name="settings">指定的通知设置。</param>
+		/// <returns>返回通知结果对象，由具体实现者定义。</returns>
+		Task<object> NotifyAsync(string name, object content, object destination, object settings = null);
+	}
+}
--- tests/Data/ScopingTest.cs
+++ tests/Data/ScopingTest.cs
@@ -26,6 +26,93 @@ namespace Zongsoft.Data
 
 			scoping.Add("!");
 			Assert.Equal(1, scoping.Count);
+
+			var members = scoping.ToArray();
+
+			Assert.Equal(1, members.Length);
+			Assert.Equal("!", members[0]);
+
+			members = scoping.ToArray(wildcard => this.GetEntityProperties());
+			Assert.Equal(0, members.Length);
+
+			scoping.Add("*, !CreatorId, ! createdTime");
+			members = scoping.ToArray(wildcard => this.GetEntityProperties());
+			Assert.True(members.Length > 1);
+			Assert.True(Array.Exists(members, item => string.Equals(item, "AssetId", StringComparison.OrdinalIgnoreCase)));
+			Assert.False(Array.Exists(members, item => string.Equals(item, "CreatorId", StringComparison.OrdinalIgnoreCase)));
+			Assert.False(Array.Exists(members, item => string.Equals(item, "CreatedTime", StringComparison.OrdinalIgnoreCase)));
+		}
+
+		private string[] GetEntityProperties()
+		{
+			return new string[]
+			{
+				"AssetId",
+				"AssetNo",
+				"PlateNo",
+				"Barcode",
+				"Name",
+				"PinYin",
+				"Spec",
+				"Size",
+				"Kind",
+				"Tags",
+				"Source",
+				"PointId",
+				"ManufacturerId",
+				"ManufacturedDate",
+				"CorporationId",
+				"BranchId",
+				"Schema",
+				"DepartmentId",
+				"AssetTypeId",
+				"AssetClassId",
+				"Grade",
+				"GradeDescription",
+				"Score",
+				"ScoreTimestamp",
+				"Flags",
+				"FlagsTimestamp",
+				"Status",
+				"StatusTimestamp",
+				"StatusDescription",
+				"PlaceId",
+				"AddressId",
+				"AddressDetail",
+				"Village",
+				"Altitude",
+				"Longitude",
+				"Latitude",
+				"Visible",
+				"ActiveTime",
+				"PlatedTime",
+				"MeasuredValue",
+				"MeasuredDescription",
+				"SummaryPath",
+				"AttachmentMark",
+				"ResponsibleId",
+				"ResponsibleName",
+				"ResponsiblePhoneNumber",
+				"OperatingVendorId",
+				"OperatingVendorName",
+				"OperatingVendorPhoneNumber",
+				"ReservedData",
+				"ReservedText1",
+				"ReservedText2",
+				"ReservedText3",
+				"ReservedText4",
+				"ReservedCount1",
+				"ReservedCount2",
+				"ReservedCount3",
+				"ReservedCount4",
+				"ReservedAmount1",
+				"ReservedAmount2",
+				"ReservedAmount3",
+				"ReservedAmount4",
+				"CreatorId",
+				"CreatedTime",
+				"Remark",
+			};
 		}
 	}
 }
