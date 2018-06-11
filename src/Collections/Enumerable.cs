387163e89167b194642ae953148c1fceacfd4708
Popeye Zhong <9555843@qq.com>
2018-06-11 21:28:19 +0800

    feat: 增加一个 Enumerable 静态类。 :elephant:

---
+++ src/Collections/Enumerable.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2018 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.CoreLibrary.
+ *
+ * Zongsoft.CoreLibrary is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.CoreLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.CoreLibrary; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Zongsoft.Collections
+{
+	public static class Enumerable
+	{
+		#region 公共方法
+		public static IEnumerable Empty(Type elementType)
+		{
+			if(elementType == null)
+				throw new ArgumentNullException(nameof(elementType));
+
+			return (IEnumerable)System.Activator.CreateInstance(typeof(EmptyIterator<>).MakeGenericType(elementType));
+		}
+		#endregion
+
+		#region 嵌套子类
+		private class EmptyIterator<T> : IEnumerable<T>
+		{
+			public IEnumerator<T> GetEnumerator()
+			{
+				yield break;
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+			{
+				yield break;
+			}
+		}
+		#endregion
+	}
+}
