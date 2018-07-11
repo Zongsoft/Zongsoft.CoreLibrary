ff266c9fe1261613f87c31b9ebc8823d58a723c3
Popeye Zhong <9555843@qq.com>
2018-07-11 17:58:36 +0800

    feat: 为 Enumerable 类增加 Enumerate(...) 方法，以支持迭代枚举器包装功能。 :guitar:

--- src/Collections/Enumerable.cs
+++ src/Collections/Enumerable.cs
@@ -38,12 +38,34 @@ namespace Zongsoft.Collections
 			if(elementType == null)
 				throw new ArgumentNullException(nameof(elementType));
 
-			return (IEnumerable)System.Activator.CreateInstance(typeof(EmptyIterator<>).MakeGenericType(elementType));
+			return (IEnumerable)System.Activator.CreateInstance(typeof(EmptyEnumerable<>).MakeGenericType(elementType));
+		}
+
+		public static IEnumerable<T> Enumerate<T>(object source)
+		{
+			if(source == null)
+				return System.Linq.Enumerable.Empty<T>();
+
+			if(source is IEnumerable<T> items)
+				return items;
+
+			return (IEnumerable<T>)System.Activator.CreateInstance(typeof(TypedEnumerable<>).MakeGenericType(typeof(T)), new object[] { source });
+		}
+
+		public static IEnumerable Enumerate(object source, Type elementType)
+		{
+			if(elementType == null)
+				throw new ArgumentNullException(nameof(elementType));
+
+			if(source == null)
+				return (IEnumerable)System.Activator.CreateInstance(typeof(EmptyEnumerable<>).MakeGenericType(elementType));
+			else
+				return (IEnumerable)System.Activator.CreateInstance(typeof(TypedEnumerable<>).MakeGenericType(elementType), new object[] { source });
 		}
 		#endregion
 
 		#region 嵌套子类
-		private class EmptyIterator<T> : IEnumerable<T>
+		private class EmptyEnumerable<T> : IEnumerable<T>
 		{
 			public IEnumerator<T> GetEnumerator()
 			{
@@ -54,6 +76,141 @@ namespace Zongsoft.Collections
 			{
 				yield break;
 			}
+		}
+
+		private class TypedEnumerable<T> : IEnumerable<T>
+		{
+			#region 私有变量
+			private Func<IEnumerator<T>> _iterator;
+			#endregion
+
+			#region 构造函数
+			public TypedEnumerable(object source)
+			{
+				var items = source as IEnumerable;
+
+				if(items != null && (source.GetType() != typeof(string) || typeof(T) == typeof(char)))
+					_iterator = () => new MultitapEnumerator(items.GetEnumerator());
+				else
+				{
+					if(Zongsoft.Common.Convert.TryConvertValue<T>(source, out var element))
+						_iterator = () => new SimulateEnumerator(element);
+					else
+						throw new InvalidOperationException($"The '{source.GetType()}' type cannot be convert to '{typeof(T)}' type.");
+				}
+			}
+			#endregion
+
+			#region 迭代遍历
+			public IEnumerator<T> GetEnumerator()
+			{
+				return _iterator();
+			}
+
+			IEnumerator IEnumerable.GetEnumerator()
+			{
+				return _iterator();
+			}
+			#endregion
+
+			#region 迭代实现
+			private class SimulateEnumerator : IEnumerator<T>
+			{
+				private int _flag;
+				private T _element;
+
+				public SimulateEnumerator(T element)
+				{
+					_element = element;
+				}
+
+				public T Current
+				{
+					get
+					{
+						if(_flag == 1)
+							return _element;
+
+						if(_flag == 0)
+							throw new InvalidOperationException("The iterator has not yet started, please call the MoveNext() method first.");
+						else
+							throw new InvalidOperationException("The iterator has terminated.");
+					}
+				}
+
+				object IEnumerator.Current
+				{
+					get
+					{
+						return this.Current;
+					}
+				}
+
+				public bool MoveNext()
+				{
+					var original = System.Threading.Interlocked.CompareExchange(ref _flag, 1, 0);
+
+					if(original == 0)
+						return true;
+
+					System.Threading.Interlocked.CompareExchange(ref _flag, 2, 1);
+					return false;
+				}
+
+				public void Reset()
+				{
+					_flag = 0;
+				}
+
+				public void Dispose()
+				{
+					if(_element is IDisposable disposable)
+						disposable.Dispose();
+				}
+			}
+
+			private class MultitapEnumerator : IEnumerator<T>
+			{
+				private IEnumerator _enumerator;
+
+				public MultitapEnumerator(IEnumerator enumerator)
+				{
+					_enumerator = enumerator;
+				}
+
+				public T Current
+				{
+					get
+					{
+						return (T)_enumerator.Current;
+					}
+				}
+
+				object IEnumerator.Current
+				{
+					get
+					{
+						return _enumerator.Current;
+					}
+				}
+
+				public bool MoveNext()
+				{
+					return _enumerator.MoveNext();
+				}
+
+				public void Reset()
+				{
+					_enumerator.Reset();
+				}
+
+				public void Dispose()
+				{
+					if(_enumerator is IDisposable disposable)
+						disposable.Dispose();
+				}
+			}
+			#endregion
 		}
 		#endregion
 	}
---
+++ tests/Collections/EnumerableTest.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Xunit;
+
+namespace Zongsoft.Collections
+{
+	public class EnumerableTest
+	{
+		[Fact]
+		public void TestEmpty()
+		{
+			var objects = Enumerable.Empty(typeof(object));
+			var strings = Enumerable.Empty(typeof(string));
+			var integers = Enumerable.Empty(typeof(int));
+			var currency = Enumerable.Empty(typeof(decimal));
+
+			Assert.NotNull(objects);
+			Assert.NotNull(strings);
+			Assert.NotNull(integers);
+			Assert.NotNull(currency);
+
+			Assert.Empty(objects);
+			Assert.Empty(strings);
+			Assert.Empty(integers);
+			Assert.Empty(currency);
+
+			Assert.IsAssignableFrom<IEnumerable<object>>(objects);
+			Assert.IsAssignableFrom<IEnumerable<string>>(strings);
+			Assert.IsAssignableFrom<IEnumerable<int>>(integers);
+			Assert.IsAssignableFrom<IEnumerable<decimal>>(currency);
+		}
+
+		[Fact]
+		public void TestEnumerate()
+		{
+			const string FIRST = "First";
+
+			var objects = Enumerable.Enumerate(new object(), typeof(object));
+			var strings = Enumerable.Enumerate(FIRST, typeof(string));
+			var integers = Enumerable.Enumerate(100, typeof(int));
+			var currency = Enumerable.Enumerate(100, typeof(decimal));
+			var dates = Enumerable.Enumerate(new[] { DateTime.MinValue, DateTime.MaxValue }, typeof(DateTime));
+			var items = Enumerable.Enumerate(new Items(), typeof(Zongsoft.Tests.IPerson));
+
+			Assert.NotNull(objects);
+			Assert.NotNull(strings);
+			Assert.NotNull(integers);
+			Assert.NotNull(currency);
+			Assert.NotNull(dates);
+			Assert.NotNull(items);
+
+			Assert.NotEmpty(objects);
+			Assert.NotEmpty(strings);
+			Assert.NotEmpty(integers);
+			Assert.NotEmpty(currency);
+			Assert.NotEmpty(dates);
+			Assert.NotEmpty(items);
+
+			Assert.IsAssignableFrom<IEnumerable<object>>(objects);
+			Assert.IsAssignableFrom<IEnumerable<string>>(strings);
+			Assert.IsAssignableFrom<IEnumerable<int>>(integers);
+			Assert.IsAssignableFrom<IEnumerable<decimal>>(currency);
+			Assert.IsAssignableFrom<IEnumerable<DateTime>>(dates);
+			Assert.IsAssignableFrom<IEnumerable<Zongsoft.Tests.IPerson>>(items);
+
+			var objectsIterator = objects.GetEnumerator();
+			var stringsIterator = strings.GetEnumerator();
+			var integersIterator = integers.GetEnumerator();
+			var currencyIterator = currency.GetEnumerator();
+			var datesIterator = dates.GetEnumerator();
+			var itemsIterator = items.GetEnumerator();
+
+			Assert.Throws<InvalidOperationException>(() => objectsIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => stringsIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => integersIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => currencyIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => datesIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => itemsIterator.Current);
+
+			Assert.True(objectsIterator.MoveNext());
+			Assert.True(stringsIterator.MoveNext());
+			Assert.True(integersIterator.MoveNext());
+			Assert.True(currencyIterator.MoveNext());
+
+			Assert.NotNull(objectsIterator.Current);
+			Assert.Equal(FIRST, stringsIterator.Current);
+			Assert.Equal(100, integersIterator.Current);
+			Assert.Equal(100m, currencyIterator.Current);
+
+			Assert.True(datesIterator.MoveNext());
+			Assert.Equal(DateTime.MinValue, datesIterator.Current);
+			Assert.True(datesIterator.MoveNext());
+			Assert.Equal(DateTime.MaxValue, datesIterator.Current);
+
+			Assert.True(itemsIterator.MoveNext());
+			Assert.IsAssignableFrom<Zongsoft.Tests.IEmployee>(itemsIterator.Current);
+			Assert.True(itemsIterator.MoveNext());
+			Assert.IsAssignableFrom<Zongsoft.Tests.ICustomer>(itemsIterator.Current);
+
+			Assert.False(objectsIterator.MoveNext());
+			Assert.False(stringsIterator.MoveNext());
+			Assert.False(integersIterator.MoveNext());
+			Assert.False(currencyIterator.MoveNext());
+			Assert.False(datesIterator.MoveNext());
+			Assert.False(itemsIterator.MoveNext());
+
+			Assert.Throws<InvalidOperationException>(() => objectsIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => stringsIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => integersIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => currencyIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => datesIterator.Current);
+			Assert.Throws<InvalidOperationException>(() => itemsIterator.Current);
+		}
+
+		private class Items : IEnumerable
+		{
+			private readonly object[] _items = new object[]
+			{
+				new Zongsoft.Tests.Employee(100, "Popeye"),
+				new Zongsoft.Tests.Customer("Sophia"),
+			};
+
+			public IEnumerator GetEnumerator()
+			{
+				return new ItemEnumerator(_items);
+			}
+
+			private class ItemEnumerator : IEnumerator
+			{
+				private int _index;
+				private object[] _items;
+
+				public ItemEnumerator(object[] items)
+				{
+					_index = -1;
+					_items = items;
+				}
+
+				public object Current
+				{
+					get
+					{
+						var index = _index;
+
+						if(index >= 0 && index < _items.Length)
+							return _items[index];
+
+						throw new InvalidOperationException();
+					}
+				}
+
+				public bool MoveNext()
+				{
+					var index = System.Threading.Interlocked.Increment(ref _index);
+
+					if(index < _items.Length)
+						return true;
+
+					System.Threading.Interlocked.Exchange(ref _index, _items.Length);
+					return false;
+				}
+
+				public void Reset()
+				{
+					_index = -1;
+				}
+			}
+		}
+	}
+}
