c2e77bbfdeb7890abb7c7afddc6def08105daa67
Popeye Zhong <9555843@qq.com>
2019-01-18 19:58:45 +0800

    算了，还是还原回去吧，不然子类用起来需要转换类型，蛋疼！ :dog:

--- src/Data/DataServiceBase.cs
+++ src/Data/DataServiceBase.cs
@@ -779,18 +779,11 @@ namespace Zongsoft.Data
 			return result;
 		}
 
-		protected virtual object OnGet(ICondition condition, ISchema schema, object state, out IPaginator paginator)
+		protected virtual TEntity OnGet(ICondition condition, ISchema schema, object state, out IPaginator paginator)
 		{
 			var result = this.DataAccess.Select<TEntity>(this.Name, condition, schema, null, state, null, ctx => this.OnGetting(ctx), ctx => this.OnGetted(ctx));
 			paginator = result as IPaginator;
-
-			using(var iterator = result.GetEnumerator())
-			{
-				if(iterator.MoveNext())
-					return iterator.Current;
-				else
-					return null;
-			}
+			return result.FirstOrDefault();
 		}
 		#endregion
 
