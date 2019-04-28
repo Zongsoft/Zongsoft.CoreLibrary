7485127bf4a4d35b4f8ae22789166d92ecbb4796
Popeye Zhong <9555843@qq.com>
2019-04-28 11:54:29 +0800

    调整代码格式。 :frog:

--- src/Data/DataAccessException.cs
+++ src/Data/DataAccessException.cs
@@ -34,40 +34,35 @@ namespace Zongsoft.Data
 	/// </summary>
 	public class DataAccessException : DataException
 	{
-		#region 成员字段
-		private int _code;
-		private string _driverName;
-		#endregion
-
 		#region 构造函数
 		public DataAccessException(string driverName, int code)
 		{
-			_code = code;
-			_driverName = driverName;
+			this.Code = code;
+			this.DriverName = driverName;
 		}
 
 		public DataAccessException(string driverName, int code, string message) : base(message)
 		{
-			_code = code;
-			_driverName = driverName;
+			this.Code = code;
+			this.DriverName = driverName;
 		}
 
 		public DataAccessException(string driverName, int code, Exception innerException) : base(null, innerException)
 		{
-			_code = code;
-			_driverName = driverName;
+			this.Code = code;
+			this.DriverName = driverName;
 		}
 
 		public DataAccessException(string driverName, int code, string message, Exception innerException) : base(message, innerException)
 		{
-			_code = code;
-			_driverName = driverName;
+			this.Code = code;
+			this.DriverName = driverName;
 		}
 
 		protected DataAccessException(SerializationInfo info, StreamingContext context)
 		{
-			_code = info.GetInt32("Code");
-			_driverName = info.GetString("DriverName");
+			this.Code = info.GetInt32(nameof(Code));
+			this.DriverName = info.GetString(nameof(DriverName));
 		}
 		#endregion
 
@@ -75,50 +70,27 @@ namespace Zongsoft.Data
 		/// <summary>
 		/// 获取数据访问的错误代码。
 		/// </summary>
-		public int Code
-		{
-			get
-			{
-				return _code;
-			}
-		}
+		public int Code { get; }
 
 		/// <summary>
 		/// 获取数据访问驱动程序的名称。
 		/// </summary>
-		public string DriverName
-		{
-			get
-			{
-				return _driverName;
-			}
-		}
-
-		/// <summary>
-		/// 获取异常消息文本。
-		/// </summary>
-		public override string Message
-		{
-			get
-			{
-				if(string.IsNullOrWhiteSpace(base.Message))
-					return Resources.ResourceUtility.GetString("Text.DataAccessException.Message", this.GetType().Assembly);
-
-				return base.Message;
-			}
-		}
+		public string DriverName { get; }
 		#endregion
 
 		#region 重写方法
 		public override string ToString()
 		{
-			return $"[{_driverName}]{_code.ToString()}" + Environment.NewLine + this.Message;
+			if(string.IsNullOrEmpty(this.Message))
+				return this.DriverName + ":" + this.Code.ToString();
+			else
+				return this.DriverName + ":" + this.Code.ToString() + Environment.NewLine + this.Message;
 		}
 
 		public override void GetObjectData(SerializationInfo info, StreamingContext context)
 		{
-			info.AddValue("Code", _code);
-			info.AddValue("DriverName", _driverName);
+			info.AddValue(nameof(Code), Code);
+			info.AddValue(nameof(DriverName), DriverName);
 		}
 		#endregion
 	}
