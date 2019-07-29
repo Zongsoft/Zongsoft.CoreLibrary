1767351655ceec92323e376356059a2edda0bb3b
Popeye Zhong <9555843@qq.com>
2019-07-29 15:34:35 +0800

    为文本序列化参数类增加静态构造方法。 :stew: :trollface:

--- src/Runtime/Serialization/TextSerializationSettings.cs
+++ src/Runtime/Serialization/TextSerializationSettings.cs
@@ -1,8 +1,15 @@
 ﻿/*
- * Authors:
- *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *   _____                                ______
+ *  /_   /  ____  ____  ____  _________  / __/ /_
+ *    / /  / __ \/ __ \/ __ \/ ___/ __ \/ /_/ __/
+ *   / /__/ /_/ / / / / /_/ /\_ \/ /_/ / __/ /_
+ *  /____/\____/_/ /_/\__  /____/\____/_/  \__/
+ *                   /____/
  *
- * Copyright (C) 2015-2017 Zongsoft Corporation <http://www.zongsoft.com>
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@qq.com>
+ *
+ * Copyright (C) 2015-2019 Zongsoft Corporation <http://www.zongsoft.com>
  *
  * This file is part of Zongsoft.CoreLibrary.
  *
@@ -43,6 +50,13 @@ namespace Zongsoft.Runtime.Serialization
 		{
 			_indented = false;
 			_namingConvention = SerializationNamingConvention.None;
+		}
+
+		public TextSerializationSettings(SerializationNamingConvention naming, bool typed = false)
+		{
+			_typed = typed;
+			_indented = false;
+			_namingConvention = naming;
 		}
 		#endregion
 
@@ -105,6 +119,24 @@ namespace Zongsoft.Runtime.Serialization
 			{
 				_namingConvention = value;
 			}
+		}
+		#endregion
+
+		#region 静态属性
+		/// <summary>
+		/// 获取一个以小驼峰命名转换的设置。
+		/// </summary>
+		public static TextSerializationSettings Camel
+		{
+			get => new TextSerializationSettings(SerializationNamingConvention.Camel);
+		}
+
+		/// <summary>
+		/// 获取一个以大驼峰命名转换的设置。
+		/// </summary>
+		public static TextSerializationSettings Pascal
+		{
+			get => new TextSerializationSettings(SerializationNamingConvention.Pascal);
 		}
 		#endregion
 	}
